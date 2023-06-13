887acf6c34e70159a9c0289705d7277bdda41bad
Christian <c.espinoza.code@gmail.com>
2023-06-13 00:05:50 -0500

    initial components created: App, Home, MyAccount, MyOrder, NorFound, SignIn

---
+++ src/Pages/MyOrder/index.jsx
@@ -0,0 +1,11 @@
+function MyOrder() {
+  return (
+    <>
+      <div className='bg-red-300'>
+        <p>MyOrder</p>
+      </div>
+    </>
+  )
+}
+
+export { MyOrder }
---
+++ src/Pages/NotFound/index.jsx
@@ -0,0 +1,11 @@
+function NotFound() {
+  return (
+    <>
+      <div className='bg-red-300'>
+        <p>NotFound</p>
+      </div>
+    </>
+  )
+}
+
+export { NotFound }
