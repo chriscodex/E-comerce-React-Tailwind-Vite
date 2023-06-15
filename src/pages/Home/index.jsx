64b4c113fa2e297edc21a6e02b07f8e91dd05d75
Christian <c.espinoza.code@gmail.com>
2023-06-14 22:05:27 -0500

    state for api consumption created

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from 'react';
 import { Layout } from '../../components/Layout';
 import { Card } from '../../components/Card';
 
 function Home() {
+  const [items, setItems] = useState(null)
+
+  const request = () => {
+
+  }
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch()
+      } catch (error) {
+        console.error(`Ocurrió un error: ${error}`)
+      }
+    }
+  }, [])
+  
   return (
     <Layout>
       <h1>Home</h1>
