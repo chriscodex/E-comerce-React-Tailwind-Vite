ce7914787c04e5172edddd0b281321a4134d23ab
Christian <c.espinoza.code@gmail.com>
2023-06-14 23:25:02 -0500

    api consumption logic implemented

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,27 +2,29 @@ import { useState, useEffect } from 'react';
 import { Layout } from '../../components/Layout';
 import { Card } from '../../components/Card';
 
+import { apiUrl } from '../../api';
+
 function Home() {
-  const [items, setItems] = useState(null)
+  const [items, setItems] = useState(null);
 
-  const request = () => {
-
-  }
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch()
-      } catch (error) {
-        console.error(`Ocurrió un error: ${error}`)
-      }
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`${apiUrl}/products`);
+      const products = await response.json();
+      setItems(products);
+      console.log(products);
+    } catch (error) {
+      console.error(`Ocurrió un error: ${error}`);
     }
-  }, [])
-  
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <Layout>
       <h1>Home</h1>
-      <Card />
     </Layout>
   );
 }
