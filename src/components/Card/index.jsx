ab4137a2abf68e0cfb19945f554baacc21c1e958
Christian <c.espinoza.code@gmail.com>
2023-06-15 16:09:45 -0500

    card '+' icon changed

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,8 +1,9 @@
 import { useContext } from 'react';
+import { PlusIcon } from '@heroicons/react/24/solid';
 import { ShoppingCartContext } from '../../context';
 
 function Card({ product }) {
-  const {count, setCount} = useContext(ShoppingCartContext);
+  const { count, setCount } = useContext(ShoppingCartContext);
 
   return (
     <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
@@ -21,7 +22,7 @@ function Card({ product }) {
             return setCount(count + 1);
           }}
         >
-          +
+          <PlusIcon className="h-6 w-6 text-black" />
         </div>
       </figure>
       <p className="flex justify-between items-center">
