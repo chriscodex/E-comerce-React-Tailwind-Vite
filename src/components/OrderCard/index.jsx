31d52ad91940be168029d4b6a2651f639c647d02
Christian <c.espinoza.code@gmail.com>
2023-06-16 18:58:06 -0500

    price & title centered - mb property added

--- src/components/OrderCard/index.jsx
+++ src/components/OrderCard/index.jsx
@@ -13,7 +13,7 @@ function OrderCard({ id, title, imageUrl, price, handleDelete }) {
         </figure>
         <p className="text-sm font-light">{title}</p>
       </div>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 mb-3">
         <p className="text-lg font-medium">{price}</p>
         {handleDelete && (
           <TrashIcon
