bbfd3f6e5ec8441a09d650235a0cdbff6a9a0a29
Christian <c.espinoza.code@gmail.com>
2023-06-16 18:39:43 -0500

    products added to MyOrder page

--- src/pages/MyOrder/index.jsx
+++ src/pages/MyOrder/index.jsx
@@ -1,13 +1,30 @@
-import { Layout } from '../../components/Layout'
+import { useContext } from 'react';
+import { ShoppingCartContext } from '../../context';
+import { Layout } from '../../components/Layout';
+import { OrderCard } from '../../components/OrderCard';
 
 function MyOrder() {
+  const {order} = useContext(ShoppingCartContext)
+  const lastOrder = order[order.length-1]
+
   return (
-    <>
-      <Layout>
-        <p>MyOrder</p>
-      </Layout>
-    </>
-  )
+    <Layout>
+      <p>My Order</p>
+      <div className="px-6 flex-1">
+        {
+          lastOrder.products.map((product) => (
+            <OrderCard
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              imageUrl={product.images[0]}
+              price={product.price}
+            />
+          ))
+        }
+      </div>
+    </Layout>
+  );
 }
 
-export { MyOrder }
+export { MyOrder };
